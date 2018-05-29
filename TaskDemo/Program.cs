a7289872248acf6d5d750e8b761c1aed5f0b2a14
ma-ak2011 <ponyonekoak@gmail.com>
2018-05-30 06:51:27 +0900

    初回コミット

---
+++ TaskDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        public static void Test1()
+        {
+            Console.WriteLine("処理開始");
+            var task = Task.Factory
+                .StartNew(() =>
+                {
+                    Thread.Sleep(5000);
+                    return new Random().Next(0, 9).ToString();
+                })
+                .ContinueWith(t =>
+                {
+                    Console.WriteLine("終了！結果は？");
+                    Console.WriteLine(t.Result);
+                });
+
+            Console.WriteLine("処理待ち");
+            task.Wait();
+
+
+        }
+    }
+}
